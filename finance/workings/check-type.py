ed1cd0cc6d1c091150deafff67874f7f3a6be6b4
Danial Othman <danialbinothman@gmail.com>
2021-07-29 09:35:52 +0000

    edit check-type.py

--- finance/workings/check-type.py
+++ finance/workings/check-type.py
@@ -1,9 +1,12 @@
 # import os
 
-shares0 = "23"
-shares1 = "21av"
-shares2 = "1.24"
-shares3 = ""
+# shares0 = "23"
+# shares1 = "21av"
+# shares2 = "1.24"
+# shares3 = ""
+
+shares = ["23", "21av", "1.24", ""]
+
 
 if shares1.isalnum() == True:
     print("shares1 is alphanumeric")
